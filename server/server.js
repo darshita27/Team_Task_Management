0c175e572294bb8263d32a10af94d9c161a6f66a
Darshita <131277268+darshita27@users.noreply.github.com>
2026-05-05 12:04:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ import { inngest, functions } from "./inngest/index.js";
 const app= express();
 app.use(express.json());
 app.use(cors())
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use(clerkMiddleware());
 app.get('/', (req, res)=> res.send('Server is live!'));
-app.use("/api/inngest", serve({ client: inngest, functions }));
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
